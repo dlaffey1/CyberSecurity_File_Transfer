4f2bb0d886d9457f72df0ef9bd0b1bd89d57aa1f
DJK101 <danieljeffersonkennedy@gmail.com>
2024-05-20 16:42:32 +0100

    Add fileEncryption locally to uploadFile.js

--- dj/static/uploadFile.js
+++ dj/static/uploadFile.js
@@ -1,27 +1,59 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("form");
+
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        console.log(event);
+    });
+});
+
 async function getFileAsArrayBuffer() {
     const file = document.getElementById("file-selector").files[0];
     const fileArray = await file.arrayBuffer();
 
-    return fileArray;
+    const encoder = new TextEncoder();
+    const fileNameArray = encoder.encode(file.name);
+    const fileTypeArray = encoder.encode(file.type);
+
+    return [fileArray, fileNameArray, fileTypeArray];
 }
 
-async function encryptFile(key) {
-    const file = await getFileAsArrayBuffer();
+async function getEncryptedFile(key) {
+    const [fileAB, fileNameAB, fileTypeAB] = await getFileAsArrayBuffer();
     const counter = window.crypto.getRandomValues(new Uint8Array(16));
-    console.log("file");
-    console.log(file);
 
-    const encrypted = await window.crypto.subtle.encrypt(
+    const encryptedFile = await window.crypto.subtle.encrypt(
         {
             name: "AES-CTR",
             counter,
             length: 64,
         },
         key,
-        file
+        fileAB
     );
 
-    return encrypted;
+    const encryptedFileName = await window.crypto.subtle.encrypt(
+        {
+            name: "AES-CTR",
+            counter,
+            length: 64,
+        },
+        key,
+        fileNameAB
+    );
+
+    const encryptedFileType = await window.crypto.subtle.encrypt(
+        {
+            name: "AES-CTR",
+            counter,
+            length: 64,
+        },
+        key,
+        fileTypeAB
+    );
+
+    return [encryptedFile, encryptedFileName, encryptedFileType, counter];
 }
 
 async function generateNewKey() {
@@ -37,10 +69,68 @@ async function generateNewKey() {
 }
 
 async function prepareFile() {
+    const encryptKeyPair = await getKeyPairFromDB("encrypt");
+    const sigKeyPair = await getKeyPairFromDB("sig");
+
     const fileKey = await generateNewKey();
-    const encryptedFile = await encryptFile(fileKey);
-    const assymKeys = await getKeyPairsFromDB("encrypt");
 
-    const encryptedFileKey = await window.crypto.subtle.wrapKey("raw", fileKey, )
+    const [encryptedFile, encryptedFileName, encryptedFileType, counter] =
+        await getEncryptedFile(fileKey);
 
+    const fileKeyAsArrayBuffer = await window.crypto.subtle.exportKey(
+        "raw",
+        fileKey
+    );
+
+    const encryptedFileKey = await window.crypto.subtle.encrypt(
+        { name: "RSA-OAEP" },
+        encryptKeyPair.publicKey,
+        fileKeyAsArrayBuffer
+    );
+
+    const fileSignature = await window.crypto.subtle.sign(
+        { name: "RSA-PSS", saltLength: 32 },
+        sigKeyPair.privateKey,
+        encryptedFile
+    );
+
+    const decryptedFile = await window.crypto.subtle.decrypt(
+        {
+            name: "AES-CTR",
+            counter,
+            length: 64,
+        },
+        fileKey,
+        encryptedFile
+    );
+    const decryptedFileName = await window.crypto.subtle.decrypt(
+        {
+            name: "AES-CTR",
+            counter,
+            length: 64,
+        },
+        fileKey,
+        encryptedFileName
+    );
+    const decryptedFileType = await window.crypto.subtle.decrypt(
+        {
+            name: "AES-CTR",
+            counter,
+            length: 64,
+        },
+        fileKey,
+        encryptedFileType
+    );
+    const decoder = new TextDecoder();
+    const newFileName = decoder.decode(decryptedFileName);
+    const newFileType = decoder.decode(decryptedFileType);
+
+    const newFile = new File([decryptedFile], newFileName, {
+        type: newFileType,
+    });
+
+    console.log("Decrypted File below");
+    console.log(newFile);
+
+    return [encryptedFileKey, counter, encryptedFile, fileSignature];
 }
