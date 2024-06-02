e3d252fc1ab78878b0e98066d8c2ef7d0fd98572
DJK101 <danieljeffersonkennedy@gmail.com>
2024-06-03 00:58:37 +0100

    Clean working directory, to only include version with frontend

--- RetrieveFile.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5000/get_file'
-data = {'p': 123, 'g': 456, 'private_key_dh': 789}  # Provide the necessary parameters for Diffie-Hellman key exchange
-response = requests.post(url, data=data)
-
-with open('File.txt', 'wb') as f:
-    f.write(response.content)
