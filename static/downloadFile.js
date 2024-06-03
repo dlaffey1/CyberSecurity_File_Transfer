1875ce503bfb96ab7cd7a076b36c4a49013bcdfe
DJK101 <danieljeffersonkennedy@gmail.com>
2024-06-03 20:40:32 +0100

    Disable download button until donload complete/fails

--- static/downloadFile.js
+++ static/downloadFile.js
@@ -1,74 +1,80 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const fileLists = await getFileLists();
-    const fileSelect = document.getElementById("file_select");
-    const allFiles = fileLists.all_files;
+  const fileLists = await getFileLists();
+  const fileSelect = document.getElementById("file_select");
+  const allFiles = fileLists.all_files;
 
-    for (let i = 0; i < allFiles.length; i++) {
-        const option = document.createElement("option");
-        option.value = i;
-        option.textContent = `${allFiles[i].label} - ${allFiles[i].owner}`;
-        fileSelect.appendChild(option);
-    }
+  for (let i = 0; i < allFiles.length; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = `${allFiles[i].label} - ${allFiles[i].owner}`;
+    fileSelect.appendChild(option);
+  }
 
-    const form = document.getElementById("form");
-    form.addEventListener("submit", async (event) => {
-        event.preventDefault();
-        const keyPassword = document.getElementById("keyPassword").value;
+  const form = document.getElementById("form");
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    document.getElementById("submit").setAttribute("disabled","");
 
-        const fileIndex = fileSelect.value;
-        await downloadAndDecryptFile(
-            allFiles[fileIndex].owner,
-            allFiles[fileIndex].label,
-            keyPassword
-        );
-    });
+    const keyPassword = document.getElementById("keyPassword").value;
+
+    const fileIndex = fileSelect.value;
+    await downloadAndDecryptFile(
+      allFiles[fileIndex].owner,
+      allFiles[fileIndex].label,
+      keyPassword
+    );
+  });
 });
 
 async function downloadAndDecryptFile(username, fileLabel, keyPassword) {
+  try {
     const response = await (
-        await fetch(`${URL_PREFIX}/downloadFile/${username}/${fileLabel}`)
+      await fetch(`${URL_PREFIX}/downloadFile/${username}/${fileLabel}`)
     ).json();
     const [encrypedFileKey, fileCounter, fileSig, encryptedFileData] =
-        unpackFileInfoFromResponse(response);
+      unpackFileInfoFromResponse(response);
 
     const publicKey = await getPublicKey(username, "sig");
 
     const signatureIsValid = await verifyEncryptedFile(
-        encryptedFileData.file,
-        fileSig,
-        publicKey
+      encryptedFileData.file,
+      fileSig,
+      publicKey
     );
 
     if (!signatureIsValid) {
-        alert("Error: File corruption. File signature is not valid");
-        return;
+      alert("Error: File corruption. File signature is not valid");
+      return;
     }
 
     let encryptPrivKey;
     try {
-        encryptPrivKey = await getPrivKeyFromDB("encrypt", keyPassword);
+      encryptPrivKey = await getPrivKeyFromDB("encrypt", keyPassword);
     } catch {
-        alert(
-            "Couldn't retrive private key. Please re-enter key wrapping password"
-        );
-        return;
+      alert(
+        "Couldn't retrive private key. Please re-enter key wrapping password"
+      );
+      return;
     }
 
     const fileKey = await decryptFileKey(encrypedFileKey, encryptPrivKey);
     const file = await decryptFileData(fileKey, fileCounter, encryptedFileData);
 
     download(file, file.name);
+  } finally {
+    document.getElementById("submit").removeAttribute("disabled");
+  }
 }
 
 function unpackFileInfoFromResponse(response) {
-    return [
-        B64ToAB(response.key),
-        B64ToAB(response.counter),
-        B64ToAB(response.sig),
-        {
-            file: B64ToAB(response.file),
-            name: B64ToAB(response.name),
-            type: B64ToAB(response.type),
-        },
-    ];
+  return [
+    B64ToAB(response.key),
+    B64ToAB(response.counter),
+    B64ToAB(response.sig),
+    {
+      file: B64ToAB(response.file),
+      name: B64ToAB(response.name),
+      type: B64ToAB(response.type),
+    },
+  ];
 }
