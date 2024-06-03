8b75235c952a7aad80b1518fab84c17949516233
DJK101 <danieljeffersonkennedy@gmail.com>
2024-06-03 03:26:49 +0100

    Change setup script to setDefaults

---
+++ setDefualts.py
@@ -0,0 +1,31 @@
+import secrets
+import os
+import sys
+
+serverEnv = len(sys.argv) > 1 and sys.argv[1] == "server"
+
+if serverEnv:
+    host = '0.0.0.0'
+    port = 2016
+    url_prefix = "/script"
+else:
+    host = '127.0.0.1'
+    port = 5000
+    url_prefix = ""
+    
+if not os.path.isdir("instance"):
+    os.mkdir("instance")
+
+secret_key = secrets.token_hex()
+with open(".env", "w") as env_file:
+    env_file.writelines(
+        [
+            f"SECRET_KEY = '{secret_key}'\n",
+            f"HOST = {host}\n",
+            f"PORT = {port}\n",
+            f"URL_PREFIX = {url_prefix}\n",
+        ]
+    )
+    
+msg = f"{"Server Environment" if serverEnv else "Development Environment"} defaults instantiated"
+print(msg)
